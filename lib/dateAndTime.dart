224d84a1f7128a9b1e8c3574daa3c4574677cf73
Nur Javier Prasetyo <nurjavierprasetyo8789@gmail.com>
2023-10-13 13:37:31 +0700

    Update!
    revisi

--- lib/dateAndTime.dart
+++ lib/dateAndTime.dart
@@ -2,9 +2,9 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage ({Key? key, required this.title}) : super(key: key);
+  const MyHomePage ({super.key, this.title});
 
-  final String title;
+  final String? title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -32,7 +32,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build (BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text(widget.title!),
       ),
       body: Center(
         child: Column(
